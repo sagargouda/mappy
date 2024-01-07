3362cb6d276172cf704437805aefdf5bead4225f
sagar <sagarc2580@gmail.com>
2024-01-07 17:51:00 +0530

    implemented leaflet library

--- mappy/script.js
+++ mappy/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -12,8 +13,44 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 //?? geo location API
-navigator?.geolocation.getCurrentPosition(function (position) {
- console.log(position);
-}, function () {
- alert('Hey your position is weak')
-})
+
+navigator.geolocation?.getCurrentPosition(
+  function (position) {
+    const { longitude, latitude } = position.coords
+    console.log(`https://www.google.co.in/maps/@${longitude},${latitude}`);
+
+
+    const coords = [latitude, longitude]
+
+const map = L.map('map').setView(coords, 17);
+    // console.log(map);
+    
+
+L.tileLayer('https://tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+    
+    map.on('click', function (mapEvent) {
+      console.log(mapEvent);
+
+      const { lat, lng } = mapEvent.latlng;
+      const coo = [lat,lng]
+      L.marker(coo).addTo(map)
+        .bindPopup(L.popup({
+          maxWidth: 250,
+          minWidth: 150,
+          autoClose: false,
+          closeOnClick: false,
+          className: 'running-popup'
+        }))
+        .setPopupContent('workout')
+  .openPopup();
+    })
+  },
+  function () {
+    alert('Could not get your position');
+  }
+)
+
+
